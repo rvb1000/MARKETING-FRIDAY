68dff1e968a0baff9b70c23f3a9d1d2d5082c813
rvb1000 <robertinopagani@gmail.com>
2021-09-13 14:04:33 -0300

    cambio de fecha

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@
 		  hour = minute * 60,
 		  day = hour * 24;
   
-	let birthday = "Oct 01, 2021 17:00:00",
+	let birthday = "Oct 15, 2021 17:00:00",
 		countDown = new Date(birthday).getTime(),
 		x = setInterval(function() {    
   
